1f75e82738e083cdb679f97ea531979a9974555b
Quasabianth <163635474+Quasabianth@users.noreply.github.com>
2024-03-20 17:07:13 +0300

    Update Points.py

--- Points.py
+++ Points.py
@@ -7,5 +7,23 @@ class Points:
         self.points = points
         self.order = order
 
+    def get_white(self) -> list[Figure.Figure]:
+        figures = []
+        for i in range(1, 9):
+            for j in range(1, 9):
+                cand = self.points[i][j]
+                if cand.figure.color == 'w':
+                    figures.append(cand.figure)
+        return figures
+    
+    def get_black(self) -> list[Figure.Figure]:
+        figures = []
+        for i in range(1, 9):
+            for j in range(1, 9):
+                cand = self.points[i][j]
+                if cand.figure.color == 'b':
+                    figures.append(cand.figure)
+        return figures
+
     def move(self):
         pass
