43164cd494884ee23bb3fb6022f9c882b8b5faa1
Quasabianth <163635474+Quasabianth@users.noreply.github.com>
2024-03-20 17:00:36 +0300

    Update Point.py

--- Point.py
+++ Point.py
@@ -2,7 +2,7 @@ import Figure
 
 
 class Point:
-    def __init__(self, pos_x: int, pos_y: int, figure: Figure.Figure):
+    def __init__(self, pos_x: int, pos_y: int, figure: Figure.Figure | None):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.figure = figure
