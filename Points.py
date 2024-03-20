40b0494905fd1c4cbe12aadb8b61a0962b1b76a5
Quasabianth <163635474+Quasabianth@users.noreply.github.com>
2024-03-20 17:00:57 +0300

    Create Points.py

---
+++ Points.py
@@ -0,0 +1,11 @@
+import Figure
+import Point
+
+
+class Points:
+    def __init__(self, points: list[list[Point.Point]], order: str):
+        self.points = points
+        self.order = order
+
+    def move(self):
+        pass
