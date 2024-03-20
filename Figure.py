ac77f9131b0ece8e52d804a5403a3227c49cba7f
Quasabianth <163635474+Quasabianth@users.noreply.github.com>
2024-03-20 17:28:23 +0300

    Update Figure.py

--- Figure.py
+++ Figure.py
@@ -37,9 +37,9 @@ class King(Figure):
     def __init__(self, color: str, pos_x: int, pos_y: int):
         Figure.__init__(self, color, pos_x, pos_y)
         self.identifier = "King"
-    
+
     def move(self) -> list[tuple[int, int, int, int]]:
-        moves = [(self.pos_x, self.pos_y, self.pos_x + dx, self.pos_y + dy) 
+        moves = [(self.pos_x, self.pos_y, self.pos_x + dx, self.pos_y + dy)
                  for dx in [-1, 0, 1]
                  if 1 <= self.pos_x + dx <= 8
                  for dy in [-1, 0, 1]
@@ -51,6 +51,14 @@ class Knight(Figure):
     def __init__(self, color: str, pos_x: int, pos_y: int):
         Figure.__init__(self, color, pos_x, pos_y)
         self.identifier = "Knight"
+
+    def move(self) -> list[tuple[int, int, int, int]]:
+        xy: list[tuple[int, int]] = [(2, 1), [1, 2], (2, -1), (1, -2), (-2, -1), (-1, -2), (-1, 2), (-2, 1)]
+        moves = [(self.pos_x, self.pos_y, self.pos_x + dx, self.pos_y + dy)
+                 for dx, dy in xy
+                 if 1 <= self.pos_x + dx <= 8
+                 if 1 <= self.pos_y + dy <= 8]
+        return moves
 
 
 class Bishop(Figure):
