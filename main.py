cf61890e87d4643d6da4d478285e7cbf6291e1a2
Nikolay Tenzin <163635474+Quasabianth@users.noreply.github.com>
2024-03-27 19:38:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import Point
 import Points
 
 if __name__ == '__main__':
-    points = Points.Points([[Point.Point(x, y, None) for x in range(1, 9)] for y in range(1, 9)], 'w')
+    points = Points.Points([[Point.Point(x, y, None) for x in range(0, 8)] for y in range(0, 8)], 'w')
