0f5f58522c02a9714a0d437922538f537e184e98
Nikolay Tenzin <163635474+Quasabianth@users.noreply.github.com>
2024-04-13 10:06:35 +0300

    Update Analise.py

--- Analise.py
+++ Analise.py
@@ -2,8 +2,20 @@ import Figure
 import Point
 import Points
 import math
+import json
 
 
 class Analise:
     def __init__(self, points: Points):
-        pass
+        self.points = points
+
+    def count_pieces(self) -> int:
+        with open("metrix.json", "r") as metrix:
+            file_metrix = metrix.read()
+        figures: dict[str: int] = json.loads(file_metrix)
+        count = 0
+        for figure in self.points.get_white():
+            count += figures[figure]
+        for figure in self.points.get_black():
+            count -= figures[figure]
+        return count
